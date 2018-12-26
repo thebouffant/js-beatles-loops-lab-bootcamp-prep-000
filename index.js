8b7364497de6f5d4d80ece520c2bb8d727871d97
Marc <marc.troughton@gmail.com>
2018-12-26 14:25:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,14 +13,6 @@ return result
 }
 
 
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice",
-  "foo",
-  "bar"
-];
 
 var number = 0
 var newFact = []
